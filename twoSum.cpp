5888627b15409a22f23883e06bb74678cfa34a42
Nahiduzzaman Talukder <167281867+Nahiduzzaman8@users.noreply.github.com>
2026-01-07 12:55:06 +0600

    twoSum worst cases

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,1 +1,35 @@
-hello
+//two sum problem with multiple solution
+
+// Worst case
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int len = nums.size();
+        for (int a=0; a<len; a++){
+            int x = target-nums[a];
+            for (int b=0; b<len; b++){
+                if (nums[b]==x){
+                    return {a, b};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    Solution sol;
+    vector<int> res = sol.twoSum(nums, target);
+    cout << res[0] << " " << res[1] << endl;
+}
+
+//tc = O(n^2)
+
+
